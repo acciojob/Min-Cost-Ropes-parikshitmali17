9d0660f2b64c5c468b90bc0e5011059ca8183f41
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-09 20:00:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function mincost(arr)
 { 
+	let narr=arr.sort()
+	sum=0;
+	forEach((ele,i)=>{
+		sum=narr[i]+narr[i+1] +sum
+		i=i+2;
+	}
+		   return sum)
+	
 //write your code here
 // return the min cost
   

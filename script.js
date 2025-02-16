28783d287463df12fb8c3348bdcae011c35daeee
Parikshit Mali <parikshitmali17@gmail.com>
2025-02-16 19:14:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
-function mincost(arr)
-{ 
-	let narr=arr.sort()
-	sum=0;
-	for(ket i=0; i<narr.length; i++){
-		sum= narr[i]+narr[i+1] +sum
-	}
-	return sum
+// function mincost(arr)
+// { 
+// 	let narr=arr.sort()
+// 	sum=0;
+// 	for(ket i=0; i<narr.length; i++){
+// 		sum= narr[i]+narr[i+1] +sum
+// 	}
+// 	return sum
 	
-//write your code here
-// return the min cost
+// //write your code here
+// // return the min cost
   
+// }
+
+// module.exports=mincost;
+
+
+function mincost(arr)
+
+{
+
+//write your code here
+
+arr.sort((a,b)=>a-b)
+
+let cost = 0
+
+//console.log(arr)
+
+while(arr.length>1){
+
+    let first = arr.shift()
+
+    let second = arr.shift()
+
+    cost+=first+second
+
+    arr.push(first+second)
+
+    arr.sort((a,b)=>a-b)
+
+  //  console.log(arr)
+
+}
+
+return cost
 }
 
 module.exports=mincost;
